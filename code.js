9d662def3fca5576844762c6b7e854de5d2088e6
Student Name <rexsutton2@gmail.cogit>
2020-03-23 15:05:39 -0500

    Finsished through exercise #7

--- code.js
+++ code.js
@@ -1,5 +1,8 @@
 // helloWorld function 
-function helloWorld() {
+function helloWorld(value) {
     return "Hello, World!";
+}
 
+function sayHello(value){
+    return 'Hello, ' + value + '!';
 }
--- tests.js
+++ tests.js
@@ -13,3 +13,25 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+describe('sayHello', function(){
+    it('should be a defined function', function() {
+        expect(typeof helloWorld).toBe('function');
+    });
+    it('should return a string when called', function() {
+        expect(typeof sayHello()).toBe("string");
+    });
+    it('should return the string "Hello, Jane!" when executed', function() {
+        expect(sayHello('Jane')).toBe("Hello, Jane!");
+    });
+    it("should never return 'undefined' when called", function() {
+        expect(sayHello()).not.toBe(undefined);
+    });
+    it('should return Hello Alex! when "Alex" is passed through', function(){
+        expect(sayHello('Alex')).toBe('Hello, Alex!');
+    });
+    it('should return Hello, Pat! when Pat is passed through', function () {
+        expect(sayHello("Pat")).toBe("Hello, Pat!")
+    });
+});
+
